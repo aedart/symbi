eba390e5194295e15747b28279032525d78775ed
Alin Eugen Deac <aedart@gmail.com>
2019-06-08 19:01:09 +0200

    change(meta): export custom errors and meta data

---
+++ packages/meta/index.js
@@ -0,0 +1,15 @@
+// Errors
+import InvalidData from './src/errors/InvalidData';
+import InvalidTarget from './src/errors/InvalidTarget';
+import MetaError from './src/errors/MetaError';
+export {
+    InvalidTarget,
+    InvalidData,
+    MetaError
+};
+
+// Meta Data
+import Data from './src/meta/Data';
+export {
+    Data
+};
