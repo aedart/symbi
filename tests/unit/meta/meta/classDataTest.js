e066c7797ef2c9a28b93c4388bcfe033bdcfeffa
Alin Eugen Deac <aedart@gmail.com>
2019-06-09 11:05:02 +0200

    test(meta): meta class data

---
+++ tests/unit/meta/meta/classDataTest.js
@@ -0,0 +1,52 @@
+'use strict';
+
+import { ClassData } from '@aedart/meta';
+
+describe("Meta Class Data", function(){
+
+    it("can create new instance", function(){
+        let target = {};
+        let data = {
+            a: true,
+            b: false
+        };
+
+        let metaClassData = new ClassData(target, data);
+
+        expect(metaClassData).toBeTruthy();
+        expect(metaClassData.target).toBe(target);
+        expect(metaClassData.data).toBe(data);
+    });
+
+    it("has no dependencies by default", function(){
+        let target = {};
+        let data = {
+            a: true,
+            b: false
+        };
+
+        let metaClassData = new ClassData(target, data);
+
+        //console.log('default dependencies', metaClassData.dependencies);
+
+        expect(metaClassData.dependencies.length).toBe(0);
+    });
+
+    it("can set and get dependencies data", function(){
+        let target = {};
+        let data = {
+            a: true,
+            b: false
+        };
+
+        let dependencies = ['a', 'b', 'c'];
+
+        let metaClassData = new ClassData(target, data);
+        metaClassData.dependencies = dependencies;
+
+        //console.log('dependencies', metaClassData.dependencies);
+
+        expect(metaClassData.dependencies).toBe(dependencies);
+    });
+
+});
