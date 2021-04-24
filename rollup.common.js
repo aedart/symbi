5df58d765902cf459b0e783aa7d9ecd8ac2bdab7
Alin Eugen Deac <aedart@gmail.com>
2021-04-24 15:54:50 +0200

    Add rullup bundler
    While babel might be good enough, it might also yield problems for some. Bundling each package should given the best use-experience.

---
+++ rollup.common.js
@@ -0,0 +1,93 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+// import replace from '@rollup/plugin-replace';
+import eslint from '@rollup/plugin-eslint';
+import babel from '@rollup/plugin-babel';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+/**
+ * Returns configured babel plugin
+ *
+ * @return {Plugin}
+ */
+const makeBabelConfig = () => {
+    return babel({ babelHelpers: 'bundled' })
+}
+
+/**
+ * Returns a "UMD (Universal Module Definition) - (browser)" export setting
+ *
+ * @param {object} config
+ *
+ * @return {object}
+ */
+export const exportUmd = function(config = {}) {
+    const plugins = [
+        peerDepsExternal(),
+        resolve(),
+        commonjs(),
+        eslint(),
+        makeBabelConfig()
+    ]
+
+    return Object.assign({
+        input: 'src/index.js',
+        output: {
+            name: 'mixins',
+            file: 'dist/index.umd.js',
+            format: 'umd',
+            sourcemap: true
+        },
+        plugins
+    }, config);
+};
+
+/**
+ * Returns a "Common Js - (main)" export setting
+ *
+ * @param {object} config
+ *
+ * @return {object}
+ */
+export const exportCommonJs = function(config = {}) {
+    const plugins =  [
+        peerDepsExternal(),
+        eslint(),
+        makeBabelConfig()
+    ];
+
+    return Object.assign({
+        input: 'src/index.js',
+        output: {
+            file: 'dist/index.cjs.js',
+            format: 'cjs',
+            sourcemap: true
+        },
+        plugins
+    }, config);
+};
+
+/**
+ * Returns a "ES Module - (module)" export setting
+ *
+ * @param {object} config
+ *
+ * @return {object}
+ */
+export const exportEsModule = function(config = {}) {
+    const plugins = [
+        peerDepsExternal(),
+        eslint(),
+        makeBabelConfig()
+    ];
+
+    return Object.assign({
+        input: 'src/index.js',
+        output: {
+            file: 'dist/index.esm.js',
+            format: 'es',
+            sourcemap: true
+        },
+        plugins
+    }, config);
+};
