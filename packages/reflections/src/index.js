efe23f2abfec463c6141666e3d06b288c4561efc
Alin Eugen Deac <aedart@gmail.com>
2021-06-26 19:31:40 +0200

    Add class meta implementation

--- packages/reflections/src/index.js
+++ packages/reflections/src/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
 import MetaReflection from './MetaReflection';
+import ClassMeta from './meta/ClassMeta';
 import MethodMeta from './meta/MethodMeta';
 import FunctionMeta from './meta/FunctionMeta';
 import ParameterMeta from './meta/ParameterMeta';
 
 export {
     MetaReflection,
+    ClassMeta,
     MethodMeta,
     FunctionMeta,
     ParameterMeta
---
+++ packages/reflections/src/meta/ClassMeta.js
@@ -0,0 +1,105 @@
+import { ClassMeta as Contract } from '@aedart/contracts';
+import mix from '@aedart/mixins';
+import MetaTarget from '../concerns/MetaTarget';
+
+/**
+ * Class Meta
+ *
+ * @implements Contract
+ */
+export default class ClassMeta extends mix(MetaTarget)
+    .inherit(Contract)
+    .make() {
+    /**
+     * Meta methods
+     *
+     * @protected
+     *
+     * @type {MethodMeta[]}
+     */
+    metaMethods;
+
+    /**
+     * ClassMeta
+     *
+     * @param {Function} target
+     * @param {MethodMeta[]} [methods]
+     */
+    constructor(target, methods = []) {
+        super();
+
+        this.defineMetaTarget(target);
+        this.metaMethods = methods;
+    }
+
+    /**
+     * Name of the class
+     *
+     * @public
+     *
+     * @return {string|null}
+     */
+    get name() {
+        const target = this.target;
+        const proto = Reflect.getPrototypeOf(target);
+        if (!proto) {
+            return null;
+        }
+
+        if (Reflect.has(proto, 'constructor') && proto.constructor.hasOwnProperty('name')) {
+            return proto.constructor.name;
+        }
+
+        return null;
+    }
+
+    /**
+     * Class methods
+     *
+     * @public
+     *
+     * @return {MethodMeta[]}
+     */
+    get methods() {
+        return this.metaMethods;
+    }
+
+    /**
+     * Class constructor method
+     *
+     * @public
+     *
+     * @return {MethodMeta|null}
+     */
+    get constructorMethod() {
+        for (const method of this.methods) {
+            if (method.isConstructor()) {
+                return method;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Determine if class has methods
+     *
+     * @public
+     *
+     * @return {boolean}
+     */
+    hasMethods() {
+        return this.methods.length > 0;
+    }
+
+    /**
+     * Determine if class has constructor method declared
+     *
+     * @public
+     *
+     * @return {boolean}
+     */
+    hasConstructor() {
+        return this.constructorMethod !== null;
+    }
+}
