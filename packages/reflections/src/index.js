0cab039a065ea03dc213e217456dfd15ccef7370
Alin Eugen Deac <aedart@gmail.com>
2021-06-26 19:03:18 +0200

    Add method meta implementation

--- packages/reflections/src/index.js
+++ packages/reflections/src/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 import MetaReflection from './MetaReflection';
+import MethodMeta from './meta/MethodMeta';
 import FunctionMeta from './meta/FunctionMeta';
 import ParameterMeta from './meta/ParameterMeta';
 
 export {
     MetaReflection,
+    MethodMeta,
     FunctionMeta,
     ParameterMeta
 };
---
+++ packages/reflections/src/meta/MethodMeta.js
@@ -0,0 +1,66 @@
+import { MethodMeta as Contract } from '@aedart/contracts';
+import FunctionMeta from './FunctionMeta';
+import mix from '@aedart/mixins';
+
+/**
+ * Class Method Meta
+ *
+ * @implements Contract
+ */
+export default class MethodMeta extends mix(FunctionMeta)
+    .inherit(Contract)
+    .make() {
+    /**
+     * Class meta that this method meta belongs to
+     *
+     * @protected
+     *
+     * @type {ClassMeta}
+     */
+    metaParent;
+
+    /**
+     * MethodMeta
+     *
+     * @param {ClassMeta} meta The class meta that this method meta belongs to
+     * @param {Function} target
+     * @param {ParameterMeta[]} parameters
+     */
+    constructor(meta, target, parameters = []) {
+        super(target, parameters);
+
+        this.metaParent = meta;
+    }
+
+    /**
+     * The class meta that this method meta belongs to
+     *
+     * @public
+     *
+     * @return {ClassMeta}
+     */
+    get parent() {
+        return this.metaParent;
+    }
+
+    /**
+     * Determine if method is class constructor
+     *
+     * @public
+     *
+     * @return {boolean}
+     */
+    isConstructor() {
+        const target = this.parent.target;
+        if (!target) {
+            return false;
+        }
+
+        const proto = Reflect.getPrototypeOf(target);
+        if (!proto) {
+            return false;
+        }
+
+        return Reflect.has(proto, 'constructor') && proto.constructor === this.target;
+    }
+}
