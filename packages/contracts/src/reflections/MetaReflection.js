2beac227648377afb538faa0d58ce2d37f32911b
Alin Eugen Deac <aedart@gmail.com>
2021-06-26 18:00:47 +0200

    Change interface, define class methods
    Still not complete, but starting to look like something

--- packages/contracts/src/reflections/MetaReflection.js
+++ packages/contracts/src/reflections/MetaReflection.js
@@ -1,12 +1,24 @@
 /**
- * Meta Reflection method symbol
+ * Meta Reflection symbol
  *
- * This symbol is used to identify a class method
- * able to define meta reflections.
+ * This symbol is used to identify a class, method or function
+ * defines meta reflection
  *
  * @type {symbol}
  */
 export const META_REFLECTION_SYMBOL = Symbol('meta-reflection');
+
+/**
+ * Class Meta Builder callback
+ *
+ * @name classBuilderCallback
+ * @function
+ *
+ * @param {Function} target Class reference
+ * @param {ClassBuilder} builder
+ *
+ * @return {void}
+ */
 
 /**
  * Meta Reflection
@@ -14,6 +26,55 @@ export const META_REFLECTION_SYMBOL = Symbol('meta-reflection');
  * @interface
  */
 export default class MetaReflection {
+    /**
+     * Get class meta
+     *
+     * @param {Function} target
+     *
+     * @return {ClassMeta|null}
+     */
+    static ofClass(target) {}
+
+    /**
+     * Get class method meta
+     *
+     * @param {Function} target
+     *
+     * @return {MethodMeta|null}
+     */
+    static ofMethod(target) {}
+
+    /**
+     * Get function meta
+     *
+     * @param {Function} target
+     *
+     * @return {FunctionMeta|null}
+     */
+    static ofFunction(target) {}
+
+    /**
+     * Determine if target has meta defined
+     *
+     * @param {Function} target
+     *
+     * @return {boolean}
+     */
+    static has(target) {}
+
+    /**
+     * Define meta for given class
+     *
+     * @param {Function} target Class reference
+     * @param {classBuilderCallback} callback
+     *
+     * @throws {TypeError}
+     */
+    static defineClass(target, callback) {}
+
+    // TODO: ...
+    static defineFunction(target, callback) {}
+
     /**
      * Returns the meta reflection symbol
      *
