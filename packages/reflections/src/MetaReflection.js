4d2d96a5aa36f7f82047a2d68d7b0733d26bf58d
Alin Eugen Deac <aedart@gmail.com>
2021-06-26 18:39:10 +0200

    Add meta reflection implementation (incomplete)

---
+++ packages/reflections/src/MetaReflection.js
@@ -0,0 +1,73 @@
+import {MetaReflection as Contract} from "@aedart/contracts";
+import {META_REFLECTION_SYMBOL} from "@aedart/contracts";
+import mix from "@aedart/mixins";
+
+/**
+ * Meta Reflection
+ *
+ * @implements Contract
+ */
+export default class MetaReflection extends mix()
+    .inherit(Contract)
+    .make() {
+
+    /**
+     * Get class meta
+     *
+     * @param {Function} target
+     *
+     * @return {ClassMeta|null}
+     */
+    static ofClass(target) {}
+
+    /**
+     * Get class method meta
+     *
+     * @param {Function} target
+     *
+     * @return {MethodMeta|null}
+     */
+    static ofMethod(target) {}
+
+    /**
+     * Get function meta
+     *
+     * @param {Function} target
+     *
+     * @return {FunctionMeta|null}
+     */
+    static ofFunction(target) {}
+
+    /**
+     * Determine if target has meta defined
+     *
+     * @param {Function} target
+     *
+     * @return {boolean}
+     */
+    static has(target) {}
+
+    /**
+     * Define meta for given class
+     *
+     * @param {Function} target Class reference
+     * @param {classBuilderCallback} callback
+     *
+     * @throws {TypeError}
+     */
+    static defineClass(target, callback) {}
+
+    // TODO: ...
+    static defineFunction(target, callback) {}
+
+    /**
+     * Returns the meta reflection symbol
+     *
+     * @public
+     *
+     * @return {symbol}
+     */
+    static get symbol() {
+        return META_REFLECTION_SYMBOL;
+    }
+}
--- packages/reflections/src/index.js
+++ packages/reflections/src/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
+import MetaReflection from "./MetaReflection";
 import ParameterMeta from "./meta/ParameterMeta";
 
 export {
+    MetaReflection,
     ParameterMeta
 }
