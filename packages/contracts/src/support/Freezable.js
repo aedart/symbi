2794b5f8b92f2640bdd6eed1e40396729695d411
Alin Eugen Deac <aedart@gmail.com>
2021-06-27 13:48:32 +0200

    Add freezable interface

---
+++ packages/contracts/src/support/Freezable.js
@@ -0,0 +1,19 @@
+/**
+ * Freezable
+ *
+ * @instance
+ * @memberOf module:support-contracts
+ */
+export default class Freezable {
+    /**
+     * Freezes this instance so that it no longer
+     * can be changed.
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
+     *
+     * @public
+     *
+     * @return {void}
+     */
+    freeze() {}
+}
--- packages/contracts/src/support/index.js
+++ packages/contracts/src/support/index.js
@@ -6,8 +6,10 @@
  * @module support-contracts
  */
 
+import Freezable from './Freezable';
 import Stringable from './Stringable';
 
 export {
+    Freezable,
     Stringable
 };
